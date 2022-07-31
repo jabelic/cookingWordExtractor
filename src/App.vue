1364c2adcaad02218ccbbc41331c173098cefa98
Jabelic <hibiki990523@gmail.com>
2022-07-31 18:47:45 +0900

    FIX: flex wrap

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Content from "./fragments/Content.vue";
 </script>
 
 <template>
-  <Content msg="Vite + Vue" />
+  <Content msg="クッキングワード抽出器【NHK東京圏】" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- src/fragments/Content.vue
+++ src/fragments/Content.vue
@@ -1,56 +1,61 @@
 <script setup lang="ts">
-import { onMounted, Ref, ref } from "vue";
+import { computed, onMounted, Ref, ref } from "vue";
 import { Program, Programs } from "../types/custom";
-// import kuromoji from "kuromoji";
 import TinySegmenter from "tiny-segmenter";
-
 defineProps({
   msg: String,
 });
 
-const areas = ref({ 東京: 130 });
-const channels = ref({ NHK総合1: "g1" });
-const area: number = areas.value["東京"];
-const channel: string = channels.value["NHK総合1"];
-const date: Date = new Date();
-const month: string =
-  date.getMonth() + 1 < 10
-    ? `0${date.getMonth() + 1}`
-    : `${date.getMonth() + 1}`;
-const today: string = `${date.getFullYear()}-${month}-${date.getDate()}`;
-
-const programs = ref<Programs>();
-const getNhkPrograms = async () =>
-  await fetch(
-    `https://api.nhk.or.jp/v2/pg/list/${area}/${channel}/${today}.json?key=${process.env.VUE_APP_NHK}`,
-    {
-      method: "GET",
-      mode: "cors",
+/** NHK */
+const useNHK = () => {
+  const areas = ref({ 東京: 130 });
+  const channels = ref({ NHK総合1: "g1" });
+  const area: number = areas.value["東京"];
+  const channel: string = channels.value["NHK総合1"];
+  const date: Date = new Date();
+  const month: string =
+    date.getMonth() + 1 < 10
+      ? `0${date.getMonth() + 1}`
+      : `${date.getMonth() + 1}`;
+  const today: string = `${date.getFullYear()}-${month}-${date.getDate()}`;
+  const programs = ref<Programs>();
+  const getNhkPrograms = async () =>
+    await fetch(
+      `https://api.nhk.or.jp/v2/pg/list/${area}/${channel}/${today}.json?key=${process.env.VUE_APP_NHK}`,
+      {
+        method: "GET",
+        mode: "cors",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => data);
+  /** 抽出された番組情報 */
+  const contents = ref<Array<{ content: string; id: string; title: string }>>();
+  const setContents = (programs: Ref<Programs>): Array<Program> => {
+    const ary = programs.value.list;
+    const contents = [];
+    for (let i = 0; i < ary[channel].length; i++) {
+      contents.push({
+        id: ary[channel][i].id,
+        title: ary[channel][i].title,
+        content: ary[channel][i].content,
+      });
     }
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      return data;
-    });
-
-const contents = ref<Array<{ content: string; id: string; title: string }>>();
-const setContents = (programs: Ref<Programs>): Array<Program> => {
-  const ary = programs.value.list;
-  const contents = [];
-  for (let i = 0; i < ary[channel].length; i++) {
-    contents.push({
-      id: ary[channel][i].id,
-      title: ary[channel][i].title,
-      content: ary[channel][i].content,
-    });
-  }
-  return contents;
+    return contents;
+  };
+  return {
+    areas,
+    channels,
+    area,
+    channel,
+    programs,
+    getNhkPrograms,
+    contents,
+    setContents,
+  };
 };
 
-const googleSearchUrl = "https://www.google.com/search?q=";
-
+/** validation */
 const isWordFood = async (word) => {
   try {
     const tmp = await fetch(
@@ -67,58 +72,70 @@ const isWordFood = async (word) => {
   } catch (e) {}
 };
 
-// const extractNoun = (tokenized) => {
-//   let noun = [];
-//   console.debug(tokenized);
-//   if ("word_list" in tokenized) {
-//     tokenized.word_list.forEach((outer) => {
-//       outer.forEach((inner) => {
-//         const word_info = {
-//           word: inner[0],
-//           part: inner[1],
-//           furigana: inner[2],
-//         };
-//         if (word_info.part == "名詞") noun.push(word_info.word);
-//       });
-//     });
-//   }
-//   console.debug(noun);
-//   return noun;
-// };
-
+/** 構文解析器 */
 const tinySegmenter = new TinySegmenter();
-const wordToExplore = ref([
-  "カニ",
-  "うに",
-  "小松菜",
-  "こまつな",
-  "胡麻豆腐",
-  "魚",
-  "牛",
-  "バナナ",
-]);
+/** 番組の内容から興味関心ワード */
+const useWordToExplore = () => {
+  const wordToExplore = ref([
+    "うに",
+    "こまつな",
+    "魚",
+    "くじら",
+    "ねぎ",
+    "たまご",
+    "カニ",
+  ]);
+  const inputVal = ref();
+  const addToWordToExplore = async () => {
+    if (!wordToExplore.value.includes(inputVal.value)) {
+      const tmp = await isWordFood(inputVal.value);
+      console.debug(tmp, inputVal.value);
+      if (tmp[0]) wordToExplore.value.push(inputVal.value);
+      else
+        alert(
+          `${inputVal.value}は食材と認識されませんでした。他のワードをお試しください。`
+        );
+    } else {
+      alert("すでに登録されています");
+    }
+    inputVal.value = "";
+  };
 
-const get_words = (sentence): string[] => {
-  const segments = tinySegmenter.segment(sentence);
-  return segments.filter((it) => {
-    return wordToExplore.value.some((wants) => it.includes(wants));
-  });
+  const get_words = (sentence): string[] => {
+    /** 分かち書きされた言葉リスト */
+    const segments = tinySegmenter.segment(sentence);
+    /** 興味関心ワードを含む単語を取り出す(配列) */
+    return segments.filter((it) => {
+      return wordToExplore.value.some((wants) => it.includes(wants));
+    });
+  };
+  return { wordToExplore, addToWordToExplore, get_words, inputVal };
 };
 
-const interestedProgs = ref([]);
+const {
+  areas,
+  channels,
+  area,
+  channel,
+  programs,
+  getNhkPrograms,
+  contents,
+  setContents,
+} = useNHK();
 
-const index = (async () => {
-  /** NHKの今日の番組一覧 */
-  programs.value = await getNhkPrograms();
-  /** タイトルと説明を抽出 */
-  contents.value = setContents(programs);
-  console.debug(contents.value);
+const { wordToExplore, addToWordToExplore, get_words, inputVal } =
+  useWordToExplore();
+
+/**興味関心ワードに一致する番組の配列 */
+const interestedProgs = ref([]);
+const getMatchProgsWithWords = () => {
   /** 興味関心ワードに一致する番組を抽出 */
   contents.value.forEach(async (item) => {
     if (item.content.length) {
       const detected = get_words(item.content);
       if (detected.length) {
         console.debug(item.title, detected);
+        if (interestedProgs.value.some((it) => it.title == item.title)) return;
         interestedProgs.value.push({
           title: item.title,
           detectedWords: detected,
@@ -126,32 +143,274 @@ const index = (async () => {
       }
     }
   });
+};
+
+const googleSearchUrl = "https://www.google.com/search?q=";
+const getGoogleSearchUrl = (arg: string) => googleSearchUrl + arg;
+const setProgs = (async () => {
+  /** NHKの今日の番組一覧 */
+  programs.value = await getNhkPrograms();
+  /** タイトルと説明を抽出 */
+  contents.value = setContents(programs);
+  /** 興味関心ワードに一致する番組を抽出 */
+  getMatchProgsWithWords();
 })();
+const currentWord = ref();
+const updateOnClick = async () => {
+  console.debug(currentWord.value);
+  await addToWordToExplore();
+  getMatchProgsWithWords();
+};
 
 onMounted(async () => {});
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <div>{{ interestedProgs }}</div>
-  <!-- <a href="http://www.goo.ne.jp/">
-    <img
-      src="//u.xgoo.jp/img/sgoo.png"
-      alt="supported by goo"
-      title="supported by goo"
-    />
-  </a> -->
-  <div class="text-center">
-    <dv-input class="w-full max-w-xs" placeholder="Type Foods!" />
+  <h2>{{ msg }}</h2>
+
+  <div class="card">
+    <div v-for="item of interestedProgs" :key="item.id" class="card-skin">
+      <div class="card__textbox">
+        <a
+          :href="getGoogleSearchUrl(item.title)"
+          target="_blank"
+          rel="noopener"
+        >
+          <div class="card__titletext">
+            {{ item.title }}
+          </div>
+          <div class="card__overviewtext">
+            <p v-for="it of item.detectedWords" :key="it">{{ it }}</p>
+          </div>
+        </a>
+      </div>
+    </div>
   </div>
-  <details>
-    <summary>Details</summary>
-    Something small enough to escape casual notice.
-  </details>
+
+  <div class="group">
+    <label for="text2">フードリストに追加します</label>
+    <div class="search_bar">
+      <div class="search_bar_content">
+        <i class="fas fa-search search_icon"></i>
+        <input
+          id="text2"
+          type="text"
+          placeholder="食材を入力"
+          v-model="inputVal"
+        />
+        <i class="fas fa-times search_icon"></i>
+      </div>
+    </div>
+  </div>
+  <div class="add_words">
+    <input type="button" value="追加" @click="updateOnClick" />
+  </div>
+
+  <div class="word_list">
+    <details class="word_list_details" open>
+      <summary>フードリスト</summary>
+      <ul>
+        <li v-for="item of wordToExplore" :key="item" class="list">
+          {{ item }}
+        </li>
+      </ul>
+    </details>
+  </div>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.word_list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.word_list_details {
+  width: 50vw;
+}
+summary {
+  position: relative;
+  display: block; /* 矢印を消す */
+  padding: 10px 10px 10px 30px; /* アイコンの余白を開ける */
+  cursor: pointer; /* カーソルをポインターに */
+  font-weight: bold;
+  background-color: #e2f0f7;
+  transition: 0.2s;
+}
+summary::hover {
+  background-color: #ccebfb;
+}
+summary::-webkit-details-marker {
+  display: none; /* 矢印を消す */
+}
+
+/* 疑似要素でアイコンを表示 */
+summary::before,
+summary::after {
+  content: "";
+  margin: auto 0 auto 10px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+}
+summary::before {
+  width: 16px;
+  height: 16px;
+  border-radius: 4px;
+  background-color: #1da1ff;
+}
+summary:after {
+  left: 6px;
+  width: 5px;
+  height: 5px;
+  border: 4px solid transparent;
+  border-left: 5px solid #fff;
+  box-sizing: border-box;
+  transition: 0.1s;
+}
+
+/* オープン時のスタイル */
+details[open] summary {
+  background-color: #ccebfb;
+}
+details[open] summary:after {
+  transform: rotate(90deg); /* アイコンを回転 */
+  left: 4px; /* 位置を調整 */
+  top: 5px; /* 位置を調整 */
+}
+
+/* アニメーション */
+details[open] .details-content {
+  animation: fadeIn 0.5s ease;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  100% {
+    opacity: 1;
+    transform: none;
+  }
+}
+
+/** ===== フードリスト ===== */
+ul,
+ol {
+  padding: 0;
+  position: relative;
+}
+
+ul li,
+ol li {
+  color: #2d8fdd;
+  border-left: solid 6px #2d8fdd; /*左側の線*/
+  background: #f1f8ff; /*背景色*/
+  margin-bottom: 3px; /*下のバーとの余白*/
+  line-height: 1.5;
+  padding: 0.5em;
+  margin: 0.1em;
+  list-style-type: none !important; /*ポチ消す*/
+}
+
+/** ===== input ===== */
+
+.group {
+  margin: 1%;
+}
+.group >>> label {
+  color: rgba(100, 100, 100, 60%);
+}
+.search_bar {
+  display: flex; /*アイコン、テキストボックスを調整する*/
+  align-items: center; /*アイコン、テキストボックスを縦方向の中心に*/
+  justify-content: center; /*アイコン、テキストボックスを横方向の中心に*/
+  flex-wrap: wrap;
+  /* height: 50px; */
+  /* width: 50vw; */
+  /* background-color: #ddd; */
+}
+
+.search_bar_content {
+  height: 50px;
+  width: 50vw;
+  background-color: #ddd;
+}
+
+.search_icon {
+  /*アイコンに一定のスペースを設ける*/
+  height: 15px;
+  width: 15px;
+  padding: 5px 5px;
+}
+
+#text2 {
+  font-size: 16px;
+  width: 95%; /*flexの中で100%広げる*/
+  height: 100%;
+  background-color: #ddd;
+  border: none; /*枠線非表示*/
+  outline: none; /*フォーカス時の枠線非表示*/
+  box-sizing: border-box; /*横幅の解釈をpadding, borderまでとする*/
+}
+
+.add_words {
+  display: flex;
+  justify-content: center;
+  margin: 2%;
+  margin-bottom: 3%;
+}
+.add_words >>> input[type="button"] {
+  width: 50vw;
+  height: 5vh;
+}
+
+/** カード */
+.card {
+  width: 90%;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+/* .card__imgframe {
+  width: 100%;
+  height: auto;
+  padding-top: 56.25%;
+  background: url(https://placehold.jp/640x360.png) no-repeat center;
+  background-size: cover;
+  box-sizing: border-box;
+} */
+.card__textbox {
+  width: 100%;
+  height: auto;
+  padding: 20px 18px;
+  background: #ffffff;
+  box-sizing: border-box;
+}
+.card__textbox > * + * {
+  margin-top: 10px;
+}
+.card__titletext {
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 125%;
+}
+.card__overviewtext {
+  font-size: 12px;
+  line-height: 150%;
+}
+.card-skin {
+  flex-flow: wrap;
+  width: 35vw;
+  overflow: hidden;
+  margin: 1%;
+  border-radius: 8px;
+  border-left: solid 1px #ddd;
+  border-right: solid 1px #ddd;
+  border-bottom: solid 5px #888;
 }
 </style>
